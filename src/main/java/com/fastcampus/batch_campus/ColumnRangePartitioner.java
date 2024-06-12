67ad50047436aded9e4dbd760143f1a6243c6be0
Reddyong <d0209hong@naver.com>
2024-06-12 11:18:42 +0900

    feat : spring batch multi thread, partioner, parallel

---
+++ src/main/java/com/fastcampus/batch_campus/ColumnRangePartitioner.java
@@ -0,0 +1,47 @@
+package com.fastcampus.batch_campus;
+
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ColumnRangePartitioner implements Partitioner {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ColumnRangePartitioner(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Map<String, ExecutionContext> partition(int gridSize) {
+        Integer min = jdbcTemplate.queryForObject("SELECT MIN(id) FROM USER", Integer.class);
+        Integer max = jdbcTemplate.queryForObject("SELECT MAX(id) FROM USER", Integer.class);
+        int targetSize = (max - min) / gridSize + 1;
+
+        Map<String, ExecutionContext> result = new HashMap<>();
+        int number = 0;
+        int start = min;
+        int end = start + targetSize - 1;
+
+        while (start <= max) {
+            ExecutionContext value = new ExecutionContext();
+            result.put("partition" + number, value);
+
+            if (end >= max) {
+                end = max;
+            }
+
+            value.putInt("minValue", start);
+            value.putInt("maxValue", end);
+            start += targetSize;
+            end += targetSize;
+            number++;
+        }
+
+        return result;
+    }
+}
